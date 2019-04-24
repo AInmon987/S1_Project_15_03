956fe4bfd7b17aec0ab30ee8fd4b8edf558dfc9f
austin inmon <Ainmon987@west-mec.org>
2019-04-24 15:18:07 -0700

    javascript commit
    When focused is changed the calcart function is ran. When onchange occurs the calCart function is run. provides validation test for selection list. Calculates the cost of the registration and saves data in session storage. when the fields are being used by the user, the information will be entered when the user switches the field. Writes data values from session storage in to the registration summary form. the users information is put on the page when entered. copied the write session Values function over to the reg2 javascript file.

--- mas_reg2.js
+++ mas_reg2.js
@@ -20,3 +20,17 @@
 
 
 */
+window.addEventListener("load", writeSessionValues);
+
+//Writes data values from session storage in to the registration summary form.
+function writeSessionValues() {
+      document.getElementById("regName").textContent = sessionStorage.getItem("confName");
+      document.getElementById("regGroup").textContent = sessionStorage.getItem("confGroup");
+      document.getElementById("regEmail").textContent = sessionStorage.getItem("confMail");
+      document.getElementById("regPhone").textContent = sessionStorage.getItem("confPhone");
+      document.getElementById("regSession").textContent = sessionStorage.getItem("confSession");
+      document.getElementById("regBanquet").textContent = sessionStorage.getItem("confBanquet");
+      document.getElementById("regPack").textContent = sessionStorage.getItem("confPack");
+      document.getElementById("regTotal").textContent = "$" + sessionStorage.getItem("confTotal");
+
+}
--- mas_register.js
+++ mas_register.js
@@ -28,29 +28,55 @@
 
 */
 window.addEventListener("load", function () {
-      //called the calcart function. 
-      calCart();
-      document.getElementById("regSubmit").onclick = sessionTest;
-      document.getElementById("fnBox").onblur = calCart;
-      document.getElementById("inBox").onblur = calCart;
-      document.getElementById("groupBox").onblur = calCart;
-      document.getElementById("mailBox").onblur = calCart;
-      document.getElementById("phoneBox").onblur = calCart;
-      document.getElementById("banquetBox").onblur = calCart;
-
-      document.getElementById("sessionBox").onchange = calCart;
-      document.getElementById("mediaCB"), onclick = calCart;
+    //called the calcart function. 
+    calCart();
+    //Runs the session test when the button is clicked.
+    document.getElementById("regSubmit").onclick = sessionTest;
+    //When focused is changed the calcart function is ran.
+    document.getElementById("fnBox").onblur = calCart;
+    document.getElementById("inBox").onblur = calCart;
+    document.getElementById("groupBox").onblur = calCart;
+    document.getElementById("mailBox").onblur = calCart;
+    document.getElementById("phoneBox").onblur = calCart;
+    document.getElementById("banquetBox").onblur = calCart;
+    //When onchange occurs the calCart function is run. 
+    document.getElementById("sessionBox").onchange = calCart;
+    document.getElementById("mediaCB"), onclick = calCart;
 });
 
 //provides validation tets for selection list. 
 function sessionTest() {
-      if (document.getElementById("sessionBox").selectedindex === -1) {
-            document.getElementById("sessionBox").setCustomValidity("Select a Session package.");
-      } else {
-            document.getElementById("sessionBox").setCustomValidity("");
-      }
+    if (document.getElementById("sessionBox").selectedindex === -1) {
+        document.getElementById("sessionBox").setCustomValidity("Select a Session package.");
+    } else {
+        document.getElementById("sessionBox").setCustomValidity("");
+    }
 }
 //Calculates the cost of the registration and saves data in session storage
 function calCart() {
-      var confNames
+    //when the fields are being used by the user, the information will be entered when the user switches the field.
+    sessionStorage.setItem("confName", document.getElementById("fnBox").value + "" + document.getElementById("lnBox").value + "");
+    sessionStorage.setItem("confGroup", document.getElementById("group").value);
+    sessionStorage.setItem("confMail", document.getElementById("email").value);
+    sessionStorage.setItem("confPhone", document.getElementById("phoneNumber").value);
+    sessionStorage.setItem("confBanquet", document.getElementById("banquetGuest").value);
+
+
+
+
+}
+//Writes data values from session storage in to the registration summary form.
+function writeSessionValues() {
+    //the users information is put on the page when entered
+    document.getElementById("regName").textContent = sessionStorage.getItem("confName");
+    document.getElementById("regGroup").textContent = sessionStorage.getItem("confGroup");
+    document.getElementById("regEmail").textContent = sessionStorage.getItem("confMail");
+    document.getElementById("regPhone").textContent = sessionStorage.getItem("confPhone");
+    document.getElementById("regSession").textContent = sessionStorage.getItem("confSession");
+    document.getElementById("regBanquet").textContent = sessionStorage.getItem("confBanquet");
+    document.getElementById("regPack").textContent = sessionStorage.getItem("confPack");
+    document.getElementById("regTotal").textContent = "$" + sessionStorage.getItem("confTotal");
+
+
+
 }
